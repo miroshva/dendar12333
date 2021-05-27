eebe38ebffc276027ea6edb87b944517de0aa48a
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-27 18:44:30 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,31 +1,52 @@
-const { MessageEmbed } = require("discord.js");
-const { LOCALE } = require("../util/EvobotUtil");
-const i18n = require("i18n");
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
 
-i18n.setLocale(LOCALE);
+  
+
 
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: i18n.__("help.description"),
+  cooldown: 8,
+  description: "**all commands Pinky**",
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle(i18n.__mf("help.embedTitle", { botname: message.client.user.username }))
-      .setDescription(i18n.__("help.embedDescription"))
-      .setColor("#F8AA2A");
+    .setThumbnail(``)
+    .setImage(``)
+    .setTitle(`Pinkt Is One`)
+    .setDescription(`
 
-    commands.forEach((cmd) => {
-      helpEmbed.addField(
-        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
-        `${cmd.description}`,
-        true
-      );
-    });
+***<a:7_:845582001544364033> | User Commands***
+${PREFIX}invite - ${PREFIX}support
+${PREFIX}about - ${PREFIX}uptime
+${PREFIX}ping - ${PREFIX}invites
+${PREFIX}avatar - ${PREFIX}se
+***<a:21:846363425155055647> | Music Commands***
+${PREFIX}play - ${PREFIX}skip
+${PREFIX}skipto - ${PREFIX}nowplaying
+${PREFIX}stop - ${PREFIX}volume
+${PREFIX}nowplaying - ${PREFIX}resume
+${PREFIX}shuffle - ${PREFIX}search
+${PREFIX}remove - ${PREFIX}queue 
+${PREFIX}filter - ${PREFIX}radio
+${PREFIX}loop - ${PREFIX}lyrics
+**<a:16:846357425343037472> | FunCommands**
+${PREFIX}prefix - ${PREFIX}ping
+${PREFIX}lock - ${PREFIX}unlock 
+${PREFIX}ban - ${PREFIX}unban
+${PREFIX}slowmode - ${PREFIX}invites
+**Links** 
+[support](https://discord.gg/m6bUgytWp9)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=784303756925468713&permissions=8&scope=bot)
+`)
 
-    helpEmbed.setTimestamp();
-
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("RANDOM")
+   message.react("✅")
     return message.channel.send(helpEmbed).catch(console.error);
+
   }
 };
