4de7eb688f5c20d5e392f994903da1da9cbc5419
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-29 11:28:20 +0300

    Update addinvite.js

--- commands/addinvite.js
+++ commands/addinvite.js
@@ -12,6 +12,6 @@ module.exports = {
         `[  Click here  ](https://discord.com/oauth2/authorize?client_id=784303756925468713&permissions=70282305&scope=bot)***invite to pore bot***
     `
       )
-      .catch(console.error);
+      message.channel.send();
   }
 };
