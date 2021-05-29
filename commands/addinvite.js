95f936df795d0cd373d909fd926aa76806d9452b
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-29 11:31:30 +0300

    Update addinvite.js

--- commands/addinvite.js
+++ commands/addinvite.js
@@ -7,7 +7,7 @@ module.exports = {
   name: "add",
   description: i18n.__('invite.description'),
   execute(message) {
-    return message.member
+    channel.message.member
       .send(
         `[  Click here  ](https://discord.com/oauth2/authorize?client_id=784303756925468713&permissions=70282305&scope=bot)***invite to pore bot***
     `
