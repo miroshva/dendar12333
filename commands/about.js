98235db2c05617e95de3a21f1af4403598cbc392
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-29 20:18:39 +0300

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -2,9 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
 var config = require('../config.json');
-
 exports.run = async (client, message, args, msg) => {
-
 const embed = new Discord.MessageEmbed()
     
 .setColor('RANDOM') 
