d1e25242c724c3e00c38e81bc2d7c3aaabc8bfaf
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-29 14:43:23 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,7 +20,8 @@ module.exports = {
     .setTitle(`Pinkt Is One`)
     .setDescription(`
 
-***<a:7_:845582001544364033> | User Commands***
+***<a:7_:845582001544364033> | User Commands**
+
 ${PREFIX}invite - ${PREFIX}support
 ${PREFIX}about - ${PREFIX}uptime
 ${PREFIX}ping - ${PREFIX}invites
@@ -38,13 +39,14 @@ ${PREFIX}shuffle - ${PREFIX}search
 ${PREFIX}remove - ${PREFIX}queue 
 ${PREFIX}filter - ${PREFIX}radio
 ${PREFIX}loop - ${PREFIX}lyrics
+
 **<a:16:846357425343037472> | FunCommands**
 
 ${PREFIX}prefix - ${PREFIX}giveway
 ${PREFIX}lock - ${PREFIX}unlock 
 ${PREFIX}ban - ${PREFIX}unban
 ${PREFIX}slowmode - ${PREFIX}invites
-**Links** 
+**Links** <a:emoji_27:847407354005291018>
 [support](https://discord.gg/m6bUgytWp9)    -    [invite](https://discord.com/api/oauth2/authorize?client_id=784303756925468713&permissions=8&scope=bot)
 `)
 
