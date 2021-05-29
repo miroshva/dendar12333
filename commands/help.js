ae22307d7a665fc7d6ac0043fcab6efe064f1757
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-29 14:47:04 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,16 +20,14 @@ module.exports = {
     .setTitle(`Pinkt Is One`)
     .setDescription(`
 
-***<a:7_:845582001544364033> | User Commands**
+***<a:7_:845582001544364033> | User Commands***
 
 ${PREFIX}invite - ${PREFIX}support
 ${PREFIX}about - ${PREFIX}uptime
 ${PREFIX}ping - ${PREFIX}invites
 ${PREFIX}avatar - ${PREFIX}se
 
-
 ***<a:21:846363425155055647> | Music Commands***
-
 
 ${PREFIX}play - ${PREFIX}skip
 ${PREFIX}skipto - ${PREFIX}nowplaying
@@ -39,7 +37,6 @@ ${PREFIX}shuffle - ${PREFIX}search
 ${PREFIX}remove - ${PREFIX}queue 
 ${PREFIX}filter - ${PREFIX}radio
 ${PREFIX}loop - ${PREFIX}lyrics
-
 **<a:16:846357425343037472> | FunCommands**
 
 ${PREFIX}prefix - ${PREFIX}giveway
