f55f97586351dfffb28cf7ceb9ac1523e3b4e256
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-29 11:23:51 +0300

    Create addinvite.js

---
+++ commands/addinvite.js
@@ -0,0 +1,17 @@
+const { LOCALE } = require("../util/EvobotUtil");
+const i18n = require("i18n");
+
+i18n.setLocale(LOCALE);
+
+module.exports = {
+  name: "invite",
+  description: i18n.__('invite.description'),
+  execute(message) {
+    return message.member
+      .send(
+        `[  Click here  ](https://discord.com/oauth2/authorize?client_id=784303756925468713&permissions=70282305&scope=bot)***invite to pore bot***
+    `
+      )
+      .catch(console.error);
+  }
+};
