f4f8d85e0ca4d742207bbd4e0a8e046be362fc65
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-31 19:48:51 +0300

    Create about.js

---
+++ commands/about.js
@@ -0,0 +1,38 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
+
+  
+
+
+module.exports = {
+  name: "add",
+  aliases: ["a"],
+  cooldown: 8,
+  description: "** Invite pore bot**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setColor('RANDOM') 
+.setThumbnail(message.author.avatarURL({dynamic: "true"}))
+.setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+.setAuthor(`Gifs About`)
+.setDescription(``)
+.addField('Servers', `\`${client.guilds.cache.size}\``, true)
+.addField('Channels', `\`${client.channels.cache.size}\``, true)
+.addField('Users', `\`${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}\``, true)
+.addField('My Name' , `\`${client.user.tag}\`` , true)
+.addField('My ID' , `\`${client.user.id}\`` , true)
+.addField('My Ping' , `\`${client.ws.ping}\`` , true)
+.addField('Owner Bot' , `<@743887896481628190> ` , true)
+
+}
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("RANDOM")
+   message.react("<a:emoji_27:847407354005291018>")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
